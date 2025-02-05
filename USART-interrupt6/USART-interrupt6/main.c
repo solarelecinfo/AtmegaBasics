4c16eb1c6b04aa34840dd3b33b3b045faa87194f
luigi <solar.elec.info@gmail.com>
2025-02-05 22:11:42 +0100

    adding good simplification code for usart

--- USART-interrupt6/USART-interrupt6/main.c
+++ USART-interrupt6/USART-interrupt6/main.c
@@ -9,8 +9,7 @@
 
 typedef int BOOLEAN;
 
-volatile unsigned int increment = 0;
-volatile BOOLEAN isMainFlowBlock = FAUX; //VRAI= blocs Data reception ISR on microcontroller.
+volatile BOOLEAN isMainFlowBlock = FAUX; //VRAI= blocs Data reception ISR on micro-controller.
 volatile int tx_read_index = 0;
 volatile int position = 0;
 char buffer[30];
@@ -22,8 +21,8 @@ void init_timer0(void);
 int main(void)
 {
 	int counter=0;
-	init_usart(25);//Enable USART rate of 2400 baud
-	init_timer0();//Enable TIMER0 interrupt with 1MHZ internal clock
+	init_usart(25); //Enable USART rate of 2400 baud
+	init_timer0();  //Enable TIMER0 interrupt with 1MHZ internal clock
 	while (1)
 	{
 		while( isMainFlowBlock == VRAI ){}
@@ -34,46 +33,43 @@ int main(void)
 	}
 }
 
-/*******************************************************************************************************/
+
+/***************************************************************************************************************/
 /* Description: Initializes the registers of the USART module in asynchronous mode.
 /* baud: Integer value written to both UBBR registers corresponding to the baud rate (bits per second).
 /* Example: 2400 baud = 25                                                               */
-/*******************************************************************************************************/
+/***************************************************************************************************************/
 void init_usart(unsigned int baud)
 {
-//Select register UBBR before start writing on UBBRH
-UBRRH &= ~(1 << URSEL);
-//MSB(HIGH) Part of register(bits 8 to 11)
-UBRRH = (unsigned char) (baud >> 8);
-//LSB(LOW) Part of register (bits 0 to 7)
-UBRRL = (unsigned char) baud;
+	//Select register UBBR before start writing on UBBRH
+	UBRRH &= ~(1 << URSEL);
+	//MSB(HIGH) Part of register(bits 8 to 11)
+	UBRRH = (unsigned char) (baud >> 8);
+	//LSB(LOW) Part of register (bits 0 to 7)
+	UBRRL = (unsigned char) baud;
 
-//Enable only transmiter module
-UCSRB = (1 << TXEN) ;
+	//Enable only transmitter module
+	UCSRB = (1 << TXEN) ;
 
-//Select register UCSR before writing on UBBRH
-//Set Parity=even ,Set stop bit=1 and total data bit length = 8
-UCSRC = (1<<URSEL)| (1<<UPM1)|(3 << UCSZ0);
-
-sei();//activate global interrupts
+	//Select register UCSR before writing on UBBRH
+	//Set Parity=even ,Set stop bit=1 and total data bit length = 8
+	UCSRC = (1<<URSEL)| (1<<UPM1)|(3 << UCSZ0);
+	sei();//activate global interrupts
 }
 
 void init_timer0(){
-DDRC =0x01; /* 0b00000001//PINC0 as OUTPUT*/
-PORTC = 0b00000000; /*initial state of pin as output*/
+	DDRC =0x01; /* 0b00000001//PINC0 as OUTPUT*/
+	PORTC = 0b00000000; /*initial state of pin as output*/
 
-//TIMER CONFIGURATION
-TCCR0 |= (1<<WGM01); //Configure the timer mode in CTC (Clear Timer on Compare Match), allowing a customizable max count value.
-TCCR0 |= (1<<COM00); //Configure the OC0 pin to toggle on each MAX_TOP value.
-TCCR0 |= (1<<CS02)|(1<<CS00);//Set the prescaler for the 8-bit timer to 1MHz / 1024.
+	//TIMER CONFIGURATION
+	TCCR0 |= (1<<WGM01); //Configure timer mode in CTC (Clear Timer on Compare Match),to allow a customizable max count value.
+	TCCR0 |= (1<<CS02)|(1<<CS00); //Set the prescalar for the 8-bit timer to (1MHz / 1024) = 976 Hz.
+	OCR0 = 244; //MAX COUNT to call interrupt and to have LED total frequency of 2Hz
 
-OCR0 = 244;//MAX_TOP counter value
-
-//INTERRUPT
-sei();
-TIMSK |= (1<<OCIE0); //Enable ISR for Timer Compare Match
+	//INTERRUPT
+	sei();
+	TIMSK |= (1<<OCIE0); //Enable ISR for Timer Compare Match
 }
-
 
 /***************************************************************************************************************/
 /* Description: Interruption called whenever DATA is ready to be sent via TX cable and interruption flag was set                                                            */
@@ -90,13 +86,9 @@ isMainFlowBlock = FAUX;
 }
 
 /***************************************************************************************************************/
-/* Description: Interruption called whenever a compare match(CTC) is reached by timer                                                         */
+/* Description: Interruption called whenever a compare match(CTC) MAX COUNT is reached by timer                                                         */
 /***************************************************************************************************************/
 ISR(TIMER0_COMP_vect){
-increment++;
-if(increment >4){
-	PORTC ^= (1<<PORTC0);
-	increment = 0;
-	status = PORTC & 0x01;
-	}
+		PORTC ^= ( 1 << PORTC0); 
+		status = PORTC & 0x01;
 }
