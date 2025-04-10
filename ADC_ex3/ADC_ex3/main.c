5a84dc12e4060c1faa5773971bbdf759f617919c
luigi <solar.elec.info@gmail.com>
2025-04-10 18:03:46 +0200

    adding adc_ex3

--- ADC_ex3/ADC_ex3/main.c
+++ ADC_ex3/ADC_ex3/main.c
@@ -1,6 +1,6 @@
 #define F_CPU 1000000UL
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
@@ -10,9 +10,10 @@ char buffer[20];
 
 //Prototype function declaration
 void usartInit(unsigned int);
-void usartTransmit(unsigned int data);
-void adcInit(void)
-void readPinAnalog(void)
+void usartTransmit(uint8_t);
+void usartTransmitString(char *);
+void adcInit(void);
+void readPinAnalog(void);
 
 int main(void)
 {
@@ -34,61 +35,47 @@ int main(void)
 	}
 }
 
-/************************************************************************/
-/*Description:Initialize ADC interface on single conversion mode(Single Analog Input)                                                                  */
-/************************************************************************/
+/**********************************************************************************
+Description:Initialize ADC interface on single conversion mode(Single Analog Input)
+************************************************************************************/
 void adcInit()
 {
 	ADMUX &= ~(1<<ADLAR);//set analog conversion to be right adjusted meaning LSB is at right of the result register
 	ADMUX |= (1<<REFS0);//To specify analog reference voltage as AVCC- typically connected to VCC
-	//Configuration frequence selonn intervales 50kHz and 200kHz
-	ADCSRA |= (1<<ADPS1)|(1<<ADPS0); //125kHz avec un prescalaire de 8
-	//Activer explicitement  le convertisseur ADC
-	ADCSRA |= (1<<ADEN);
-	// Sélection du pin d'entrée analogique PA1 (ADC1)---explicite
-	ADMUX &= 0xE0;
-	ADMUX |= 0x01;
+    ADMUX |= 0x01;//Set analog input(ADC1) with no Gain 
+	
+	ADCSRA |= (1<<ADPS1)|(1<<ADPS0); //Divide reference 1MHZ frequency by prescalar to set ADC frequency 1MHz/8= 125kHz note: ADC range must be [ 50kHz to 200kHz]
+	ADCSRA |= (1<<ADEN);//Enable ADC interface.
 }
 
-/************************************************************************/
-/* Description:Execute punctual conversion on analog input voltage
-/************************************************************************/
+/************************************************************************
+Description:Execute punctual conversion on analog input voltage 
+************************************************************************/
 void readPinAnalog(){
 	ADCSRA |= ( 1 << ADSC );//Start conversion explicitly
 	while ( ADCSRA & (1 << ADSC) ){} //Wait for conversion to finish
-	//Read ADC result register right adjusted
-	uint8_t lowAdcValue = ADCL;
-	uint16_t tenBitsResult = ADCH<<8 | lowAdcValue;
-	adcValue = tenBitsResult;
+	
+	uint8_t lowAdcValue = ADCL; //Read ADC result register(LSB) right adjusted
+	uint16_t tenBitsResult = ADCH<<8 | lowAdcValue; //Read remaining part(MSB) of ADC result register
+	adcValue = tenBitsResult; //Result ADC 10 bits ADCH(2)-ADCL(8)
 }
 
-/************************************************************************/
-/* Description: Initialize USART asynchronous interface at 2400bauds=25                                                                    */
+/************************************************************************
+Description: Initialize USART asynchronous interface at 2400bauds=25                                                                    */
 /************************************************************************/
 void usartInit (unsigned int baud)
 {
-	//Expliciter l'utilisation du registre UBBRH avant operation d'?criture sur UBBRH
-	UBRRH &= ~(1 << URSEL);
-	//Partie MSB du registre HAUT (bits 8 to 11)
-	UBRRH = (unsigned char) (baud >> 8);
-	//Partie LSB du registre BAS (bits 0 ? 7)
-	UBRRL = (unsigned char) baud;
-	//Expliciter l'utilisation du registre UCSRC avant operation d'?criture sur UBBRH
-	//Set enable EVEN PARITY
-	//Set 1 stop bits and data bit length is 8-bit
-	UCSRC = (1<<URSEL)| (1<<UPM1)|(3 << UCSZ0);
-	//Enable the receiver and transmitter
-	UCSRB =  (1 << TXEN);
-	//Enable ADC interrupt de reception
-	//UCSRB |= ( 1 << RXCIE);
-	//Enable global interrupt
-	//sei();
+	UBRRH &= ~(1 << URSEL);//Enable UBBRH write mode
+	UBRRH = (unsigned char) (baud >> 8);//Write baud speed on UBRR register MSB(bits 8 to 11)
+	UBRRL = (unsigned char) baud;//Write baud speed on UBRR register LSB(bits 0 to 7).
+	UCSRC = (1<<URSEL)| (1<<UPM1)|(3 << UCSZ0);//set parity=EVEN, stop_bit=1,data_length=8bits.
+	UCSRB =  (1 << TXEN);	//Enable the receiver and transmitter.
 }
 
 
-/************************************************************************/
-/*Description:To send a String of characters via USART interface one character at a time.                                                                 */
-/************************************************************************/
+/****************************************************************************************
+Description:To send a String of characters via USART interface one character at a time.
+****************************************************************************************/
 void usartTransmitString(char *args)
 {
 	while (*args) {
@@ -96,14 +83,13 @@ void usartTransmitString(char *args)
 	}
 }
 
-void usartTransmit (unsigned int data)
+/************************************************************************
+Description: Send data via USART asynchronous interface
+************************************************************************/
+void usartTransmit (uint8_t data)
 {
-	//UCSRB &= ~(1 << RXEN); //desactive le recepteur
-	//Wait until the Transmitter is ready
-	while (! (UCSRA & (1 << UDRE)) );
-	//Get that data outa here!
-	UDR = data;
-	//UCSRB |= (1 << TXEN);//activer le recepteur
+	while (! (UCSRA & (1 << UDRE)) ){};	//Wait until the Transmitter is ready
+	UDR = data;	//Transmit data out 
 	_delay_ms(500);
 }
 
